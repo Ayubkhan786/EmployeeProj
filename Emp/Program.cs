fd6f66f2485863e94e80c083f466bb2a892bab6d
your Name <mohamedabdulayubm@gmail.com>
2022-06-15 17:39:14 +0530

    add

--- Emp/Program.cs
+++ Emp/Program.cs
@@ -1,18 +1,34 @@
-﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+﻿Console.WriteLine("Welcome to Employee Wage until it's reached a month");
 
-int empPresent = 1;
-Random num = new Random();
-int attendance = num.Next(0, 2);
-if (attendance == empPresent)
+int RATE_HR = 20;
+int MAX_DAYS = 20;
+int MAX_HRS = 100;
+
+int empHrs = 0, maxHrs = 0, maxDays = 0;
+
+while (maxHrs <= MAX_HRS && maxDays < MAX_DAYS)
 {
+    maxDays++;
+    
+    Random check = new Random();
+    int run = check.Next(0, 3);
+    switch (run)
+    {
+        case 0:
+            empHrs = 0;
+            break;
+        case 1:
+            empHrs = 8;
+            break;
+        case 2:
+            empHrs = 4;
+            break;
+    }
+    maxHrs += empHrs;
+    Console.WriteLine("Day : " + maxDays +"\n "+ "Hours : " + maxHrs);
 
-
-    Console.WriteLine("Is Present");
-
-
+    int totWage = maxHrs * RATE_HR;
+    Console.WriteLine("The total salary is :" + totWage);
 }
 
-else
-{
-    Console.WriteLine("Is Absent");
-}
+
