f98e6b023331cd7acb50bb38bbc6e17aa22859f0
your Name <mohamedabdulayubm@gmail.com>
2022-06-15 15:46:36 +0530

    added

---
+++ Emp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+int empPresent = 1;
+Random num = new Random();
+int attendance = num.Next(0, 2);
+if (attendance == empPresent)
+{
+
+
+    Console.WriteLine("Is Present");
+
+
+}
+
+else
+{
+    Console.WriteLine("Is Absent");
+}
